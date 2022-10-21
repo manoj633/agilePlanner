1d2b82e703d9be15beecca5785e081a941687d7f
Manoj <manoj09633@gmail.com>
2022-10-21 22:37:32 +0530

    Task service created

--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+  tasksChanged = new Subject<Task[]>();
+  private tasks: Task[] = [];
+
   constructor() { }
+
+  getTasks() {
+    return this.tasks.slice();
+  }
+
+  setTasks(tasks: Task[]) {
+    this.tasks = tasks;
+    this.tasksChanged.next(this.tasks.slice());
+  }
+
+  getTask(id: number) {
+    return this.tasks.slice()[id];
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  updateTask(index: number, newTask: Task) {
+    this.tasks[index] = newTask;
+    this.tasksChanged.next(this.tasks.slice());
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+    this.tasksChanged.next(this.tasks.slice());
+  }
 }
