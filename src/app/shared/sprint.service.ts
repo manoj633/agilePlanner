991eb5e77d108f223b1e81e4be3ed7917b3586ee
Manoj <manoj09633@gmail.com>
2022-10-22 22:01:05 +0530

    Sprint service created and updated

--- src/app/shared/sprint.service.ts
+++ src/app/shared/sprint.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Sprint } from '../model/sprint.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SprintService {
 
+  sprintsChanged = new Subject<Sprint[]>();
+  private sprints: Sprint[] = [];
   constructor() { }
+
+  getSprints() {
+    return this.sprints.slice();
+  }
+
+  setSprints(sprintsReceived: Sprint[]) {
+    this.sprints = sprintsReceived;
+    this.sprintsChanged.next(this.sprints);
+  }
+
+  getSprint(id: number) {
+    return this.sprints.slice()[id];
+  }
+
+  setSprint(sprintReceived: Sprint) {
+    this.sprints.push(sprintReceived);
+  }
+
+  updateSprint(index: number, newSprint: Sprint) {
+    this.sprints[index] = newSprint;
+    this.sprintsChanged.next(this.sprints);
+  }
+
+  deleteSprint(index: number) {
+    this.sprints.slice(index, 1);
+    this.sprintsChanged.next(this.sprints)
+  }
+
+  changeSprintStatus(id: number, status: string) {
+    this.sprints[id].status = status;
+  }
 }
